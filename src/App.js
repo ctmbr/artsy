a897ef751b8702984c77bfc44b2ec64a969ef85b
Charles K.S <ctmbr@protonmail.com>
2022-08-01 20:21:57 -0700

    Added Header component and linked to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import "./App.css";
 import * as React from "react";
+import Header from "./components/Header";
 
 import { Box, extendTheme, ChakraProvider } from "@chakra-ui/react";
 
@@ -16,7 +17,7 @@ const theme = extendTheme({ colors });
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <header className="App-header"></header>
+      <Header />
       <h1>Profile</h1>
       <div>
         <p>img</p>
