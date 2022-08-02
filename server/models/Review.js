423e16be318e24de8993cbc2842f8936d9db547a
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-08-01 21:30:31 -0700

    add updates to models files

--- server/models/Review.js
+++ server/models/Review.js
@@ -2,27 +2,27 @@ const { Schema } = require('mongoose');
 const moment = require('moment');
 
 const reviewSchema = new Schema(
-  {
-    reviewBody: {
-      type: String,
-      required: true,
-      maxlength: 280
+    {
+        reviewBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
+        }
     },
-    username: {
-      type: String,
-      required: true
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: timestamp => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
+    {
+        toJSON: {
+            getters: true
+        }
     }
-  },
-  {
-    toJSON: {
-      getters: true
-    }
-  }
 );
 
 module.exports = reviewSchema;
