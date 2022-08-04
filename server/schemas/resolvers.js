1cddf006d1e7872e8f32661dbd08c7e28f19f4de
Simona <simona.snapk@gmail.com>
2022-08-03 19:47:16 -0700

    resolver code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,72 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User, Product, Review } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const resolvers = {
     Query: {
-        products: async (parent, { name }) =>
+        user: async (parent, args, context) =>
         {
-            const params = {};
+            if (context.user)
+            {
+                const user = await User.findById(context.user._id).populate({
+                });
+
+                user.products.sort((a, b) => b.releaseDate - a.releaseDate);
+
+                return user;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+        product: async (parent, { _id }) =>
+        {
+            return await Product.findById(_id).populate("shop");
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) =>
+        {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        updateUser: async (parent, args, context) =>
+        {
+            if (context.user)
+            {
+                return await User.findByIdAndUpdate(context.user._id, args, { new: true })
+            }
+
+            throw new AuthenticationError("You are not logged in");
+        },
+        updateProduct: async (parent, args, context) =>
+        {
+            if (context.product)
+            {
+                return await Product.findByIdAndUpdate(context.product._id, args, { new: true })
+            }
+        },
+        login: async (parent, { email, password }) =>
+        {
+            const user = await User.findOne({ email });
+
+            if (!user)
+            {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+
+            const correctPw = await User.isCorrectPassword(password);
+
+            if (!correctPw)
+            {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
         }
     }
 };
