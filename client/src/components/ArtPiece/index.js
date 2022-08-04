9f69d4160bfabdf680dd1084818f19819d5d5775
Mia Borja <apk.borja@gmail.com>
2022-08-03 19:13:56 -0700

    added two components for stripe testing

---
+++ client/src/components/ArtPiece/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ArtPiece(item) {
+    const {
+        name,
+        description,
+        image,
+        price,
+        quantity,
+        category,
+    } = item;
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ArtPiece;
---
+++ client/src/components/ArtworkList/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+function ArtworkList() {
+    const { loading, data } = useQuery();
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ArtworkList;
