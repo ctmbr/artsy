e88de2a19a26f752d296927880d06293c5ef708c
Mia Borja <apk.borja@gmail.com>
2022-08-03 21:16:47 -0700

    added placeholders for reducers

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,24 +15,25 @@ export const LOGIN = gql`
 
 export const ADD_USER = gql`
     mutation addUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-    ) 
-    {
+        $firstName: String!
+        $lastName: String!
+        $email: String!
+        $password: String!
+    ) {        
         addUser(
-        firstName: $firstName
-        lastName: $lastName
-        email: $email
-        password: $password
-        )
-        {
+            firstName: $firstName
+            lastName: $lastName
+            email: $email
+            password: $password
+        ) {
             token
-            user
-            {
+            user {
                 _id
             }
         }
     }
-`
+`;
+
+export const UPDATE_USER = gql`
+
+`;
