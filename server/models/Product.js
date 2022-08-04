edd19e55840196fbf52f33f748ef3ce4b89c347a
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-08-03 20:23:26 -0700

    revised format

--- server/models/Product.js
+++ server/models/Product.js
@@ -3,39 +3,47 @@ const dateFormat = require("../utils/dateFormat");
 
 const { Schema } = mongoose;
 
-const productSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String
-    },
-    image: {
-        type: String
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 0.99
-    },
-    releaseDate: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => dateFormat(timestamp),
-    },
-    //   quantity: {
-    //     type: Number,
-    //     min: 0,
-    //     default: 0
-    //   },
-    //   category: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: "Category",
-    //     required: true
-    //   }
-});
+const productSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        description: {
+            type: String
+        },
+
+        image: {
+            type: String
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            min: 0.99
+        },
+
+        releaseDate: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+
+        //   quantity: {
+        //     type: Number,
+        //     min: 0,
+        //     default: 0
+        //   },
+
+        //   category: {
+        //     type: Schema.Types.ObjectId,
+        //     ref: "Category",
+        //     required: true
+        //   }
+    }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
