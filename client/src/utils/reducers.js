81c8ed515ae412e82e50f57f72a19345dd0254af
Mia Borja <apk.borja@gmail.com>
2022-08-03 19:51:34 -0700

    removed test components and added empty code for reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,5 +2,13 @@ import { useReducer } from 'react';
 import {  } from './actions';
 
 export const reducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state;
 
+    }
 };
+
+export function useArtReducer(initialState) {
+    return useReducer(reducer, initialState)
+}
