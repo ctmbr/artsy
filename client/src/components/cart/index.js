abdc9989775e7db3f6082f439f338efd6ee44a5e
Mia Borja <apk.borja@gmail.com>
2022-08-06 11:33:44 -0700

    updated cartitem with placeholder for testing

--- client/src/components/cart/index.js
+++ client/src/components/cart/index.js
@@ -3,11 +3,17 @@ import { loadStripe } from '@stripe/stripe-js';
 import { QUERY_CHECKOUT } from '../../utils/actions';
 import CartItem from '../cartItem';
 import Auth from '../../utils/auth';
+import { Container } from '@chakra-ui/react';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-
+    return (
+        <Container>
+            <p>So cool!</p>
+                <CartItem />
+        </Container>
+    );
 };
 
 export default Cart;
--- client/src/components/cartItem/index.js
+++ client/src/components/cartItem/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { REMOVE_FROM_CART } from "../../utils/actions";
+import { useArtContext } from "../../utils/globalState";
 
-const CartItem = ({ item }) => {
+const CartItem = () => {
 
+    const [, remove] = useArtContext();
+
+    const removeFromCart = item => {
+      remove({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });  
+    };
+
+    return (
+        <p>Yippee!!</p>
+    )
 };
 
 export default CartItem;
