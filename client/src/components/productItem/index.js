d94c96ae5db70a96f92315a7303858174594f57d
Mia Borja <apk.borja@gmail.com>
2022-08-11 12:14:20 -0700

    fixed error with duplicate keys in the productlist

--- client/src/components/productItem/index.js
+++ client/src/components/productItem/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { useArtContext } from "../../utils/GlobalState";
 import { UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-import { Image, Heading, Container, Box, Button, VStack } from "@chakra-ui/react";
+import { Image, Heading, Container, Box, Button, VStack, WrapItem, Center } from "@chakra-ui/react";
 import "./style.css";
 
 function ProductItem(item)
@@ -42,22 +42,26 @@ function ProductItem(item)
   }
 
   return (
-    <Container className="card px-1 py-1">
-      <VStack spacing="8pt">
-        <Link to={`/products/${_id}`}>
-          <Image src={`/images/${image}`} alt={name} />
-          <Heading size="xl">{name}</Heading>
-          <p>{description}</p>
-          {quantity < 1 && <p> OUT OF STOCK </p>}
-        </Link>
-        <Box>
-          <span>${price}</span>
-        </Box>
-        <Button colorScheme="blue" onClick={addToCart}>
-          Add to cart
-        </Button>
-      </VStack>
-    </Container>
+    <WrapItem className="card px-1 py-1">
+      <Center>
+        <Container>
+          <VStack spacing="8pt">
+            <Link to={`/products/${_id}`}>
+              <Image src={`/images/${image}`} alt={name} />
+              <Heading size="xl">{name}</Heading>
+              <p>{description}</p>
+              {quantity < 1 && <p> OUT OF STOCK </p>}
+            </Link>
+            <Box>
+              <span>${price}</span>
+            </Box>
+            <Button colorScheme="blue" onClick={addToCart}>
+              Add to cart
+            </Button>
+          </VStack>
+        </Container>
+      </Center>
+    </WrapItem>
   );
 };
 
